7e4e756ac7308fb7295cd7a24b149c623accbb12
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-03-01 21:01:28 +1000

    Command line arguments added

--- sosa.py
+++ sosa.py
@@ -19,7 +19,9 @@ def main():
     parser.add_argument("-username", dest="url", help="Provide a username for the tenant. Ex: reception@test.com")
     parser.add_argument("-password", dest="id", help="Provide a password for the tenant. Ex: hunter1.")
     parser.add_argument("--quiet", dest="quiet",  action="store_true", help="Supress banner and headers to limit to comma dilimeted results only.")
-
+    parser.add_argument("--listsites", dest="listsites",  action="store_true", help="List all sites in the provided tenant that the credentials have access to.")
+    parser.add_argument("--listdepth", dest="listdepth",  help="List the depth of subsites that should be listed. Significant performance decrease and increased noise and scanning times should be expected")
+    parser.add_argument("--listusers", dest="listusers",  help="List all users and their properties from the user information list.")
     arguments = parser.parse_args()
 
     if len(sys.argv) == 1:
